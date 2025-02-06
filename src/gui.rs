6281277c0e892f60f44d8b2f74489944612e194b
Nikki Zivkov <nikkizivkov@169-231-142-205.wireless.ucsb.edu>
2025-02-06 11:32:20 -0800

    Created gui template

---
+++ src/gui.rs
@@ -0,0 +1,75 @@
+// Nikki Zivkov 02/06/2025
+// This script generates a template for the gui 
+// Will be filled in later with actual gui elements 
+// Called to in main 
+use eframe::{egui, App, NativeOptions, CreationContext};
+
+#[derive(Default)]
+pub struct MyApp {
+    current_pane: Pane,
+}
+
+#[derive(PartialEq)]
+enum Pane {
+    Settings,
+    Readout,
+    Pump,
+    Test,
+}
+
+impl Default for Pane {
+    fn default() -> Self {
+        Pane::Settings
+    }
+}
+
+impl App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+            ui.heading("Menu");
+
+            if ui.button("Settings").clicked() {
+                self.current_pane = Pane::Settings;
+            }
+            if ui.button("Readout").clicked() {
+                self.current_pane = Pane::Readout;
+            }
+            if ui.button("Pump Tone Generation").clicked() {
+                self.current_pane = Pane::Pump;
+            }
+            if ui.button("Test Pane").clicked() {
+                self.current_pane = Pane::Test;
+            }
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            match self.current_pane {
+                Pane::Settings => {
+                    ui.heading("Settings");
+                    ui.label("More info to come");
+                }
+                Pane::Readout => {
+                    ui.heading("Readout");
+                    ui.label("Fill this in later");
+                }
+                Pane::Pump => {
+                    ui.heading("Pump Tone Generation");
+                    ui.label("Fridge Stuff ");
+                }
+                Pane::Test => {
+                    ui.heading("Test Pane");
+                    ui.label("Sample Pane");
+                }
+            }
+        });
+    }
+}
+
+pub fn run_gui() {
+    let native_options = NativeOptions::default();
+    eframe::run_native(
+        "Reading Rainbow",
+        native_options,
+        Box::new(|_cc: &CreationContext| Box::new(MyApp::default())),
+    );
+}
