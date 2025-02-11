eb76a3c672ca883023b5e9e91186fb48995dc3fa
Nikki Zivkov <nikkizivkov@169-231-143-228.wireless.ucsb.edu>
2025-02-11 11:49:42 -0800

    Commented status.rs

--- src/status.rs
+++ src/status.rs
@@ -1,26 +1,29 @@
 // Nikki Zivkov 02/10/2025
 // Readout status
 
+// Defining Status struct 
 pub struct Status {
-    pub status_message: String,
+    pub status_message: String, // Status message will be string 
 }
 
+// Implimenting methods for Status struct
 impl Status {
     pub fn new() -> Self {
         Self {
-            status_message: String::from("Disconnected"),
+            status_message: String::from("Disconnected"), // Creating default message, will be replaced 
         }
     }
 
+    // Method for updating status (will be filled in)
     #[allow(dead_code)]
     pub fn update(&mut self, message: &str) {
         self.status_message = message.to_string();
     }
 }
 
-// Implementing Default trait for Status
+// Implementing default for Status struct 
 impl Default for Status {
     fn default() -> Self {
-        Self::new()
+        Self::new() // Creating new default instance so we can keep rechecking status 
     }
 }
